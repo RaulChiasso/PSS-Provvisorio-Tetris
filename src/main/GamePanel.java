39817811087e060332cba828c7a34e8f9bc79423
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2023-12-06 18:33:04 +0100

    Add settings for JPanel

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,18 @@
 package main;
 
-public class GamePanel {
-    
+import java.awt.Dimension;
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+    public static final int WIDTH=1280;
+    public static final int HEIGHT=720;
+
+    public GamePanel() {
+
+        //Panel settings
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.black);
+        this.setLayout(null);
+    }
 }
