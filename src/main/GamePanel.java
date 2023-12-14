3896841ba26db8f1ff0d3765942f8cde1a6774af
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2023-12-14 16:43:34 +0100

    Add GameLoop for refresh the panel

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Color;
 import javax.swing.JPanel;
 
@@ -26,5 +27,33 @@ public class GamePanel extends JPanel implements Runnable{
 
     @Override
     public void run() {
+
+        //Gameloop
+        //update and repaint the panel 60 times per seconds
+        double drawIntervental = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while(gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawIntervental;
+            lastTime = currentTime;
+
+            if(delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
     }
 }
