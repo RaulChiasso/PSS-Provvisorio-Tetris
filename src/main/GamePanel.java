f10f027ca92e086fe7ab71c30f6255427a4bce9e
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2023-12-14 16:34:37 +0100

    Add Thread for launch the game

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -4,9 +4,12 @@ import java.awt.Dimension;
 import java.awt.Color;
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements Runnable{
     public static final int WIDTH=1280;
     public static final int HEIGHT=720;
+
+    final int FPS = 60;
+    Thread gameThread;
 
     public GamePanel() {
 
@@ -14,5 +17,14 @@ public class GamePanel extends JPanel{
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.black);
         this.setLayout(null);
+    }
+
+    public void launchGame() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
     }
 }
