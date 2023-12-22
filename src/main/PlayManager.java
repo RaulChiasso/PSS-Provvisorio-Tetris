a3a2d552e5dbc6d9a9520e56c6b8115b4a7cab77
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2023-12-22 17:54:06 +0100

    Draw next block window

--- src/main/PlayManager.java
+++ src/main/PlayManager.java
@@ -2,7 +2,9 @@ package main;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 public class PlayManager {
     public static final int BORDER=4;
@@ -33,6 +35,14 @@ public class PlayManager {
         g2.setColor(Color.white);
         g2.setStroke(new BasicStroke(4f));
         g2.drawRect(left_x-BORDER, top_y-BORDER, WIDTH+BORDER*2, HEIGHT+BORDER*2);
+
+        //Draw next block window
+        int x = right_x + 100;
+        int y = bottom_y - 200;
+        g2.drawRect(x, y, 200, 200);
+        g2.setFont(new Font("Arial", Font.PLAIN, 30));
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2.drawString("NEXT", x+60, y+60);
     }
         
 }
